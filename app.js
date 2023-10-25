af59bdb890905ae4a540938bc6bc71a12da210ec
Sandip <sandip.bagale34@gmail.com>
2023-10-25 19:58:53 +0545

    add blogs,singleBlog

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const app = require("express")();
+
 const mongoose = require("mongoose");
 const { connectDatabase } = require("./database/database");
+const Blog = require("./model/blogModel");
 
+const express = require("express")
+const app = express();
+ 
+//nodejs lai form bata aako data parse gar vaneko ho
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//Database connection function 
 
 connectDatabase();
 
+//GET API 
 
 app.get("/",(req,res)=>{
     res.json({
@@ -13,7 +24,100 @@ app.get("/",(req,res)=>{
     })
 })
 
+//Get API => /blogs(All blogs)
+
+app.get("/blogs",async(req,res)=>{
+
+    //fetching/reading all BLogs from Blog model
+    const blogs = await Blog.find()
+
+    //check if blogs contains data or not
+
+    if(blogs.length == 0){
+        res.status(404).json({
+            // status:404,
+            message: "Empty blogs",
+            
+        })
+    }else{
+        res.status(200).json({
+            // status:200,
+            message: "Blogs fetched successfully",
+            data : blogs
+        })
+    }
+
+   
+})
+
+//GET API ->  /blogs/:id (single Blog)
+
+app.get("/blogs/:id",async(req,res)=>{
+  const id = req.params.id
+  // const {id} = req.params 
+
+// if(blog.length == 0){
+//     res.status(404).json({
+//         message:"No blogs found with that id"
+//     })
+// }
+// else{
+//  res.status(200).json({
+//     message : "blog fetched successfully",
+//     data: blog
+//  })
+// }
+
+//Alternative
+const blog = await Blog.findById(id) 
+if(blog){
+    res.status(200).json({
+        message : "blog fetched successfully",
+        data: blog
+    })
+} else{
+    res.status(404).json({
+        message: "no blog found"
+    })
+}
+
+})
+
+
+
+//Create blog api
+
+app.post("/createBlog", async (req,res)=>{
+    const title = req.body.title;
+    const subTitle = req.body.subTitle;
+    const description = req.body.description;
+
+    //Alternative
+
+    // const {title,subTitle,description}= req.body
+
+
+    //Insert to database logic goes here
+      await  Blog.create({
+            title : title,
+            subTitle : subTitle,
+            description : description
+        })
+    
+    res.json({
+        status : 200,
+        message : "Blog Created Successfully"
+    })
+
+    //Alternative
+
+    // res.status(200).json({
+    //     message : "Blog created successfully"
+    // })
+
+})
+
 app.listen(2000,()=>{
     
-console.log("Node JS has started at port 3000")
+console.log("Node JS has started at port 2000")
 })
